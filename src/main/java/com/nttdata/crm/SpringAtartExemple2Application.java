9aafed2297fecb6c6c6650891526a3dc7be456be
Tiago Filipe Almeida <tfilipal@emeal.nttdata.com>
2023-10-17 14:18:51 +0100

    spring_start_exemple2-01

---
+++ src/main/java/com/nttdata/crm/SpringAtartExemple2Application.java
@@ -0,0 +1,13 @@
+package com.nttdata.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAtartExemple2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAtartExemple2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/crm/SpringAtartExemple2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAtartExemple2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
